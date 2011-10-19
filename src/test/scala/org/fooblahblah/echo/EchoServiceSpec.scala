c32d04218f621bfb12063133dd57801f44b0acd1
Jeff Simpson <jeff@socialmedia.com>
2011-10-18 21:36:15 -0600

    Spec still borked

--- src/test/scala/org/fooblahblah/echo/EchoServiceSpec.scala
+++ src/test/scala/org/fooblahblah/echo/EchoServiceSpec.scala
@@ -1,49 +1,35 @@
 package org.fooblahblah.echo
 
 import blueeyes.core.data.{ ByteChunk, BijectionsChunkJson, BijectionsChunkString, BijectionsChunkByteArray }
-import blueeyes.core.http.HttpHeaders._
 import blueeyes.core.http.HttpStatusCodes._
-import blueeyes.core.http.MimeTypes._
 import blueeyes.core.service.RestPathPatternImplicits._
 import blueeyes.core.service.test.BlueEyesServiceSpecification
-
 
 object EchoServiceSpec extends BlueEyesServiceSpecification with EchoService with BijectionsChunkString {
 
   override val configuration = """
     services {
       echo {
-        v1 {
-        }
+	v1 {
+	}
       }
     }
 
     log {
       level = "debug"
       console = true
-
-      filterXLightWeb  {
-        node = "org.xlightweb"
-        level = "info"
-      }
-      filterXSocket  {
-        node = "org.xsocket"
-        level = "info"
-      }
     }
     """
 
-  "EchoService" should {
+  "Echo service" should {
 
     "return posted content as response" in {
       val expected = "Foo"
-      println("before post")
       val response = service.post[String]("/echo")(expected)
-      println("response = " + response)
       response.value must eventually(beSomething)
       response.value.get.status.code must beEqual(OK)
+      response.value.get.content must beSome(expected)
     }
 
   }
 }
-
